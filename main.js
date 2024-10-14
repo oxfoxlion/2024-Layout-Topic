b7dc613293bbbe15b0faf5663c3f2ff45f064509
oxfoxlion <oxfoxlion@gmail.com>
2024-10-14 21:05:35 +0800

    更新功能頁面

--- main.js
+++ main.js
@@ -1,3 +1,54 @@
 import './assets/scss/all.scss';
 
-console.log("Hello world!");
+// GSAP
+const hoverUps = document.querySelectorAll(".hoverUp");
+
+hoverUps.forEach(hoverUp => {
+  hoverUp.addEventListener('mouseenter', () => {
+      gsap.to(hoverUp, { y:-10 });
+  });
+
+  hoverUp.addEventListener('mouseleave', () => {
+      gsap.to(hoverUp, { y:0 });
+  });
+});
+
+// AOS
+AOS.init();
+
+//shpping Cart
+let cartCount = 0;
+
+// 當按下「Add Item」按鈕時，增加購物車數量
+// document.getElementById('addCartOriginal').addEventListener('click', function() {
+//   cartCount++;
+//   updateCartBadge();
+// });
+
+// document.getElementById('addCartCocoa').addEventListener('click', function() {
+//   cartCount++;
+//   updateCartBadge();
+// });
+
+// document.getElementById('addCartTea').addEventListener('click', function() {
+//   cartCount++;
+//   updateCartBadge();
+// });
+
+// document.getElementById('addCartMiniMix').addEventListener('click', function() {
+//   cartCount++;
+//   updateCartBadge();
+// });
+
+document.querySelectorAll('.addShoppingCart').forEach(button => {
+  button.addEventListener('click', function() {
+    cartCount++;
+    updateCartBadge();
+  });
+});
+
+// 更新徽章數字
+function updateCartBadge() {
+  const cartBadge = document.getElementById('cartBadge');
+  cartBadge.textContent = cartCount > 99 ? '99+' : cartCount;
+}
