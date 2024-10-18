78a2a4eef6c7bfa2b0f02acd139dbc8ba6b7d741
oxfoxlion <oxfoxlion@gmail.com>
2024-10-18 18:27:09 +0800

    修正RWD Space Font-size

--- main.js
+++ main.js
@@ -59,3 +59,36 @@ function updateCartBadge() {
   const cartBadge = document.getElementById('cartBadge');
   cartBadge.textContent = cartCount > 99 ? '99+' : cartCount;
 }
+
+// header隱藏
+// 選取 header 和第二個區塊
+const header = document.getElementById('header');
+const headerFade = document.getElementById('headerFade');
+
+// 監聽滾動事件
+window.addEventListener('scroll', () => {
+    const headerFadeTop = headerFade.getBoundingClientRect().top;
+
+    // 當滾動到 headerFade 的位置時，隱藏 header
+    if (headerFadeTop <= 0) {
+        header.classList.add('hidden');
+    } else {
+        header.classList.remove('hidden');
+    }
+});
+
+//收藏按鈕-可可
+document.getElementById('toggleRemove').addEventListener('click', function () {
+  var targetButton = document.getElementById('targetRemove');
+  var isPressed = targetButton.getAttribute('aria-pressed') === 'true';
+  
+  // 切換狀態
+  targetButton.setAttribute('aria-pressed', !isPressed);
+  
+  // 根據狀態切換按鈕的外觀
+  if (isPressed) {
+    targetButton.classList.remove('active');
+  } else {
+    targetButton.classList.add('active');
+  }
+});
